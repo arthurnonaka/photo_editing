70f1b6926e61ef153b3f006e3107863c507617b3
Nonaka <anonaka97@gmail.com>
2021-07-11 11:08:59 -0300

    Just some coding which do nothing

---
+++ human_counting_photo.py
@@ -0,0 +1,55 @@
+import cv2
+import imutils
+import argparse
+import numpy as np
+
+def detect(frame):
+    bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.03)
+
+    person = 1
+    for x, y, w, h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+        person += 1
+
+    cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.imshow('output', frame)
+
+    return frame
+
+def detectByPathImage(path, output_path):
+    image = cv2.imread(path)
+
+    image = imutils.resize(image, width = min(800, image.shape[1]))
+
+    result_image = detect(image)
+
+    if output_path is not None:
+        cv2.imwrite(output_path, result_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def humanDetector(args):
+    image_path = args["image"]
+
+    print('[INFO] Opening Image from path.')
+    detectByPathImage(image_path, args['output'])
+
+def argsParser():
+    arg_parse = argparse.ArgumentParser()
+    arg_parse.add_argument("-i", "--image", default="photo_02.jpeg", help="path to Image File ")
+    arg_parse.add_argument("-o", "--output", type=str, help="path to optional output video file")
+    args = vars(arg_parse.parse_args())
+
+    return args
+
+if __name__ == "__main__":
+    HOGCV = cv2.HOGDescriptor()
+    HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    args = argsParser()
+    humanDetector(args)
+
+
